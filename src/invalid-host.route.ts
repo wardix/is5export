08650052c27ea89a490be4b95ba1fc0ee5a2351d
Wardi <wardi@nusa.net.id>
2023-12-20 20:49:54 +0700

    fix: check ap host pattern match with the ip address

--- src/invalid-host.route.ts
+++ src/invalid-host.route.ts
@@ -2,16 +2,14 @@ import { FastifyInstance, FastifyRequest } from 'fastify'
 import { RowDataPacket } from 'mysql2'
 
 interface Row extends RowDataPacket {
-  host: string,
+  host: string
   pop: string
 }
 
 export const invalidHostRoute = async (fastify: FastifyInstance) => {
   fastify.get('/', async (request: any, reply) => {
     if (!request.query.branch) {
-      return reply
-        .status(400)
-        .send('Missing required query parameters: branch')
+      return reply.status(400).send('Missing required query parameters: branch')
     }
     const { branch: branchParam } = request.query as {
       branch: string
@@ -21,17 +19,34 @@ export const invalidHostRoute = async (fastify: FastifyInstance) => {
     const branches = branchParam.split(',').map((x) => x.trim())
     const branchPlaceholder = branches.map((_) => '?').join(', ')
 
-    const apSql = `SELECT TRIM(npa.host) host, npc.value pop
+    const apSql = `SELECT TRIM(npa.host) host, TRIM(npa.ip) ip, npc.value pop
        FROM noc_pop_ap npa 
        LEFT JOIN noc_pop np ON npa.pop_id = np.id 
        LEFT JOIN noc_pop_custom npc ON npc.pop_id = np.id AND npc.attribute = 'Code'
        WHERE np.branch_id IN (${branchPlaceholder}) 
-       AND NOT (TRIM(npa.host) REGEXP 'ap[0-9]{3}\.[a-z]{3}')
        AND npa.shown = 1 
        AND np.shown = 1`
     const [apRows] = await connection.query<Row[]>(apSql, branches)
-    for (const { host, pop } of apRows) {
-      invalidHosts.push(`invalid_host{host="${host}",pop="${pop}"} 1`)
+    for (const { host, ip, pop } of apRows) {
+      if (!host.startsWith('ap')) {
+        invalidHosts.push(`invalid_host{host="${host}",pop="${pop}"} 1`)
+        continue
+      }
+      const isHostEndWithThreeLetters = /^[a-z]{3}$/.test(host.substring(6, 9))
+      if (!isHostEndWithThreeLetters) {
+        invalidHosts.push(`invalid_host{host="${host}",pop="${pop}"} 1`)
+        continue
+      }
+      const ipParts = ip.split('.')
+      const ipLastPart = ipParts[ipParts.length - 1]
+      const ipPadding =
+        ipLastPart.length == 1 ? '00' : ipLastPart.length == 2 ? '0' : ''
+      const validHostIpPart = `${ipPadding}${ipLastPart}`
+      if (host.substring(2, 5) === validHostIpPart) {
+        invalidHosts.push(
+          `invalid_host{host="${host}",ip="${ip}",pop="${pop}"} 1`
+        )
+      }
     }
 
     const swSql = `SELECT TRIM(nps.host) host, npc.value pop
